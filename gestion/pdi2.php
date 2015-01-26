ce392f735f8bc2af759746a881285ee9c81ce422
Sergio Cerda Zúñiga <sergio.cerda.zuniga@gmail.com>
2015-01-26 01:57:38 -0300

    cambios 26/01
    no se puede elegir un periodo menor al seleccionado en un periodo
    anterior

--- gestion/pdi2.php
+++ gestion/pdi2.php
@@ -6,7 +6,27 @@
 		<link rel="StyleSheet" type="text/css" href="gestion.css">
 		<script src="../general/ayuda.js" type="text/javascript" language="JavaScript"></script>
 		<script src="../general/prototype.js" type="text/javascript" language="JavaScript"></script>
-	</head>
+        <script type="text/javascript" languaje="JavaScript">
+            function desactivar1(id1,id2) {
+                var final = document.getElementById(id1.id).value;
+                for(var i=1; i<=final; i++){
+                    document.getElementById(id2.id).options[i].disabled = true;
+                }
+                for(var j=parseInt(final)+1; j<=54; j++){
+                    document.getElementById(id2.id).options[j].disabled = false;
+                }
+            }
+            function desactivar2(id1,id2) {
+                var final = document.getElementById(id1.id).value;
+                for(var i=1; i<=parseInt(final)+1; i++){
+                    document.getElementById(id2.id).options[i+1].disabled = true;
+                }
+                for(var j=parseInt(final)+2; j<=54; j++){
+                    document.getElementById(id2.id).options[j].disabled = false;
+                }
+            }
+        </script>
+ 	</head>
 	<body>
 		<div id="fecha">
 			<?php echo date("d/m/Y"). "<br/>" . date("H:i");?>
@@ -59,8 +79,8 @@
 							$ID_hora1 = "SELECT  `id_periodo` FROM  `periodos`";
 							$hora2 = mysql_query($hora1) or die('Consulta fallida: ' . mysql_error());
 							$ID_hora2 = mysql_query($ID_hora1) or die('Consulta fallida: ' . mysql_error());
-							echo "<select name='horario_dia_id".$j."[$i][0]'>\n";
-							echo "<option value=''>Seleccione el horario</option>\n";
+							echo "<select name='horario_dia_id".$j."[$i][0]' id='horario".$j.$i."0' onchange='desactivar1(horario".$j.$i."0,horario".$j.$i."1)'>\n";
+							echo "<option value='-1'>Seleccione el horario</option>\n";
 							while (($hora3 = mysql_fetch_array($hora2, MYSQL_ASSOC)) and ($ID_hora3 = mysql_fetch_array($ID_hora2, MYSQL_ASSOC))) {
 								foreach (array_combine($hora3,$ID_hora3) as $hora4=>$ID_hora4) {
                                     if($ID_hora4 != 0){
@@ -76,8 +96,8 @@
 							$ID_hora1 = "SELECT  `id_periodo` FROM  `periodos`";
 							$hora2 = mysql_query($hora1) or die('Consulta fallida: ' . mysql_error());
 							$ID_hora2 = mysql_query($ID_hora1) or die('Consulta fallida: ' . mysql_error());
-							echo "<select name='horario_dia_id".$j."[$i][1]'>\n";
-							echo "<option value=''>Seleccione el horario</option>\n";
+							echo "<select name='horario_dia_id".$j."[$i][1]' id='horario".$j.$i."1' onchange='desactivar2(horario".$j.$i."1,horario".$j.$i."2)'>\n";
+							echo "<option value='-1'>Seleccione el horario</option>\n";
 							while (($hora3 = mysql_fetch_array($hora2, MYSQL_ASSOC)) and ($ID_hora3 = mysql_fetch_array($ID_hora2, MYSQL_ASSOC))) {
 								foreach (array_combine($hora3,$ID_hora3) as $hora4=>$ID_hora4) {
                                     if($ID_hora4 != 0){
@@ -93,8 +113,8 @@
 							$ID_hora1 = "SELECT  `id_periodo` FROM  `periodos`";
 							$hora2 = mysql_query($hora1) or die('Consulta periodos: ' . mysql_error());
 							$ID_hora2 = mysql_query($ID_hora1) or die('Consulta fallida: ' . mysql_error());
-							echo "<select name='horario_dia_id".$j."[$i][2]'>\n";
-							echo "<option value=''>Seleccione el horario (opcional)</option>\n";
+							echo "<select name='horario_dia_id".$j."[$i][2]' id='horario".$j.$i."2'>\n";
+							echo "<option value='-1'>Seleccione el horario (opcional)</option>\n";
 							while (($hora3 = mysql_fetch_array($hora2, MYSQL_ASSOC)) and ($ID_hora3 = mysql_fetch_array($ID_hora2, MYSQL_ASSOC))) {
 								foreach (array_combine($hora3,$ID_hora3) as $hora4=>$ID_hora4) {
 									   echo "<option value='$ID_hora4'>$hora4</option>\n";
